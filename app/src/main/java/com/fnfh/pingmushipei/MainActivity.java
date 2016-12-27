fb9d2d2eb2b39e3b547c20c4eadafc177084fe2f
hyl <411136528@qq.com>
2016-12-27 22:05:58 +0800

    Initial commit

---
+++ app/src/main/java/com/fnfh/pingmushipei/MainActivity.java
@@ -0,0 +1,30 @@
+package com.fnfh.pingmushipei;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+
+    }
+
+
+}
